aac23b0d245007f4340be1487e323116c57fe215
stolaine <sheshank238mukul@gmail.com>
2021-06-07 00:22:26 +0530

    Searching in all districts of Delhi for dose two

--- implementation/search_slots.py
+++ implementation/search_slots.py
@@ -6,20 +6,28 @@ import winsound
 
 post = "POST"
 get = "GET"
-mobile = "9631496224"
 secret = "U2FsdGVkX1+YLRftb9lThlR8kBkTdtAvQz7C18IOvvonY4PPnuGDsi605yM5nHQdOloecjxuVsyhPtGUp+rp3A=="
 user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
 headers = {
     'User-Agent': user_agent
 }
 website = "cdn-api.co-vin.in"
+conn = http.client.HTTPSConnection(website)
 
-def get_sessions_by_district(district_id, date):
-    conn = http.client.HTTPSConnection(website)
-    endpoint = "/api/v2/appointment/sessions/public/findByDistrict?district_id="+district_id+"&date="+date
+def get_sessions_by_district(id, date):
+    '''
+    curl --location --request GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=650&date=22-05-2021" --header "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
+
+    '''
+    endpoint = "/api/v2/appointment/sessions/public/findByDistrict?district_id="+id+"&date="+date
+    # endpoint = "/api/v2/appointment/sessions/public/findByPin?pincode="+id+"&date="+date
     payload = ""
     conn.request(get, endpoint, payload, headers)
     response = conn.getresponse()
+    code = response.getcode()
+    if code != 200:
+        print(code)
+        return None
     sessions = json.loads(response.read().decode("utf-8"))['sessions']
     return sessions
 
@@ -27,26 +35,52 @@ def analyze_sessions(sessions):
     i = 1
     for session in sessions:
         min_age_limit = session['min_age_limit']
-        fee_type = session['fee_type']
-        # if min_age_limit == 18 and fee_type == 'Free':
         if min_age_limit == 18:
-            dose_one_capacity = session['available_capacity_dose1']
-            if dose_one_capacity > 19:
+            dose_two_capacity = session['available_capacity_dose2']
+            district_name = session['district_name']
+            fee = session['fee_type']
+            pincode = session['pincode']
+            vaccine = session['vaccine']
+            if fee == 'Free' and vaccine == 'COVAXIN' and dose_two_capacity > 0:
+                print()
                 center_name = session['name']
                 frequency = 2500
                 duration = 1000
                 winsound.Beep(frequency, duration)
-                print(i, "--", dose_one_capacity, "--", center_name, "--", datetime.datetime.now())
+                print(i, "--", dose_two_capacity, "--", district_name, ',', pincode, ',', center_name, ',', datetime.datetime.now())
                 i += 1
 
+def get_district_ids(id):
+    endpoint = '/api/v2/admin/location/districts/' + id
+    payload = ""
+    conn.request(get, endpoint, payload, headers)
+    response = conn.getresponse();
+    ids = []
+    districts = json.loads(response.read().decode('utf-8'))['districts']
+    for district in districts:
+        id = district['district_id']
+        ids.append(id)
+    return ids
 
-def get_slots():
+
+def get_slots(ids):
     while True:
         print("Searching", datetime.datetime.now())
-        date = datetime.date.today().strftime("%d-%m-%Y")
-        sessions = get_sessions_by_district("650", date)
-        analyze_sessions(sessions)
-        time.sleep(10)
+        initial_date = datetime.date.today()
+        for i in range (0,6):
+            current_date = (initial_date + datetime.timedelta(days=i)).strftime("%d-%m-%Y")
+            print(current_date, end=' -- ')
+            for id in ids:
+                print(id, end=' ')
+                sessions = get_sessions_by_district(str(id), current_date)
+                if sessions != None:
+                    analyze_sessions(sessions)
+                else:
+                    continue
+            print()
+        print()
+        time.sleep(5)
 
 if __name__ == "__main__":
-    get_slots()
+    ids = get_district_ids('9')
+    get_slots(ids)
